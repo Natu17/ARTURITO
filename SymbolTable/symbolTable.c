c460aa3001250d949d3a955c279731a1fd54e966
Natu17 <nlilienthal@itba.edu.ar>
2021-11-28 10:37:58 -0300

    add free

--- SymbolTable/symbolTable.c
+++ SymbolTable/symbolTable.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include "symbolTable.h"
 
-#define CAPACITY 50000      // Capacity of the Hash Table
+#define CAPACITY 51200      // Capacity of the Hash Table
 
 symbolTable * sTable;
 
